a8052837a037523095a1868cad7253cba1cfdaff
Eddie Ciccone <eddie@nakamotomining.ai>
2024-09-20 11:44:34 -0500

    return attention mask

--- logicnet/finetune/finetune.py
+++ logicnet/finetune/finetune.py
@@ -58,11 +58,11 @@ def preprocess_function(examples):
 
     # Tokenize the input and the expected response
     model_inputs = tokenizer(inputs, truncation=True,
-                             max_length=702, padding="max_length")
+                             max_length=702, padding="max_length", return_attention_mask=True)
 
     # Tokenize the expected response (label)
     labels = tokenizer(
-        examples['response'], truncation=True, max_length=702, padding="max_length")
+        examples['response'], truncation=True, max_length=702, padding="max_length", return_attention_mask=True)
     model_inputs["labels"] = labels["input_ids"]
 
     return model_inputs
